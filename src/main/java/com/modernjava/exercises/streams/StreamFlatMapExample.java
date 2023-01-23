38cbcf331d0a7a6322c8b4f9f62f0e8be9be2662
Marilene Soares da Costa <cmarilene@vmware.com>
2023-01-22 19:59:09 -0700

    adds examples using .distinct(), .count() and .sorted() to StreamFlatMapExample

--- src/main/java/com/modernjava/exercises/streams/StreamFlatMapExample.java
+++ src/main/java/com/modernjava/exercises/streams/StreamFlatMapExample.java
@@ -12,7 +12,15 @@ public class StreamFlatMapExample {
     public static List<String> printStudentActivities(){
         List<String> studentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
                 .map(Student::getActivities) // Stream<List<String>>
+//                .peek(student -> {
+//                    System.out.println("peek1 :" +student);
+//                })
                 .flatMap(List::stream) // Stream<String>
+//                .peek(student -> {
+//                    System.out.println("peek2 Mapping each activity in each list :" +student);
+//                })
+                .distinct()
+                .sorted()
                 .collect(Collectors.toList());
         return studentActivities;
     }
@@ -20,15 +28,32 @@ public class StreamFlatMapExample {
     public static Set<String> printStudentActivitiesSet(){
         Set<String> studentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
                 .map(Student::getActivities) // Stream<List<String>>
+                .peek(activitiesList -> {
+                    System.out.println("peek1 Mapping each list :" +activitiesList);
+                })
                 .flatMap(List::stream) // Stream<String>
+                .peek(activity -> {
+                    System.out.println("peek2 Mapping each activity in each list :" +activity);
+                })
                 .collect(Collectors.toSet());
         return studentActivities;
     }
 
+    public static long getStudentActivitiesCount(){
+        long numberOfStudentActivities = StudentDataBase.getAllStudents().stream() // Stream<Student>
+                .map(Student::getActivities) // Stream<List<String>>
+                .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> with distinct function performed
+                .count();
+        return numberOfStudentActivities;
+    }
+
+
 
     public static void main(String[] args) {
         // goal - print all activities within the lists
-        System.out.println("activities List: " + printStudentActivities());;
-        System.out.println("activities Set :" + printStudentActivitiesSet());;
+        System.out.println("activities List: " + printStudentActivities());
+//        System.out.println("activities Set :" + printStudentActivitiesSet());
+        System.out.println("number of activities: " + getStudentActivitiesCount());
     }
 }
